7f7a1603c2e5378cfebf852c23ea4e757d578ed8
gajewsk2 <gajewsk2@illinois.edu>
2017-01-26 21:08:19 -0600

    Initial commit. quickly getting multi fields working

---
+++ jp.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright: Damien Elmes <anki@ichi2.net>
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+# Modified to be awesome-er by Micah Gajewski
+import japanese.model
+import japanese.reading
+import japanese.lookup
+import japanese.stats
+import japanese.bulkreading
